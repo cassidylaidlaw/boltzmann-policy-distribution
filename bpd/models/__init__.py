34982b7e69727a2508958ffbce0c43a466101858
Cassidy Laidlaw <cassidy256@gmail.com>
2022-04-20 15:52:07 -0700

    Forgot to commit another change fixing issues when Overcooked isn't installed

--- bpd/models/__init__.py
+++ bpd/models/__init__.py
@@ -1,2 +1,6 @@
-from . import overcooked_models  # noqa: F401
 from . import pickup_ring_models  # noqa: F401
+
+try:
+    from . import overcooked_models  # noqa: F401
+except ImportError:
+    pass  # Might fail if Overcooked isn't installed.
